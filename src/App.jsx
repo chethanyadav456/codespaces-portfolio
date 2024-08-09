8385a76169de81a33a05b4ecf09ce62c338dc11f
M Chethan Yadav <rockchethan1111@gmail.com>
2024-08-09 18:01:04 +0000

    made changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "M Chethan Yadav",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "chethanyadav456@gmail.com",
+  gitHub: "chethanyadav456",
+  instagram: "chethanyadav.me",
+  linkedIn: "chethanyadav",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Chethan0x",
+  youTube: "CodingWithChethan",
 };
 
 const primaryColor = "#4E567E";
